e72363a7d6f96f67cfb77ec3143a7f256a8abe71
prince-sinha-dev <pk3214975@gmail.com>
2025-08-12 10:08:12 +0530

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -49,5 +49,11 @@ app.use("/api/messages", messageRouter)
 
 // connect to mongodb
 await connectDB();
+
+if(process.env.NODE_ENV !=="production"){
 const PORT = process.env.PORT || 5000;
 server.listen(PORT ,()=>console.log("server is runing on PORT :" + PORT));
+}
+
+export default server;
+
